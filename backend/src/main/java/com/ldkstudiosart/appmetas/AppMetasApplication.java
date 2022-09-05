d52afb5b64032904e0f8de2d95209a7487e7f25c
leandrodklein <leklein@gmail.com>
2022-09-05 20:00:57 -0300

    Project created,

---
+++ backend/src/main/java/com/ldkstudiosart/appmetas/AppMetasApplication.java
@@ -0,0 +1,13 @@
+package com.ldkstudiosart.appmetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMetasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ldkstudiosart/appmetas/AppMetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldkstudiosart.appmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
